5141788063941ca69f995ee46df9e4bb452cb699
audesilva <audesilva@gmail.com>
2014-11-22 07:04:32 +0000

    test for both NFC and relay

--- test/test_sic4310.c
+++ test/test_sic4310.c
@@ -8,6 +8,7 @@
 
 /* Includes -----------------------------------------------------------------------*/
 #include "sic4310.h"
+#include "relay.h"
 #include "util.h"
 
 /* Private types ------------------------------------------------------------------*/
@@ -24,12 +25,26 @@
 int main()
 {
     uint8_t byte;
+    uint8_t activate_relay = RELAY_CH1 | RELAY_CH5;
     
+    relay_config(activate_relay);
+    timebase_config();
     SIC4310_config();
     timebase_config();
     while(1) {
         while (SIC4310_available()) {
             byte = SIC4310_read();
+						if (byte == 00){
+							relay_turn_off(RELAY_CH1);
+							delay(1000);
+								
+						}
+						else if (byte == 01){
+							relay_turn_on(RELAY_CH1);
+							delay(1000);
+						
+						}
+											
             delay(100);
             SIC4310_write(byte);
         }
